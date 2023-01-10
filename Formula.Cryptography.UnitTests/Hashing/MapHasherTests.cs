4beb77f644fea6a009cad44f59191e56c96ef16a
Raiford Brookshire <formulacoder@outlook.com>
2023-01-10 15:57:00 -0600

    Added MapHasher

---
+++ Formula.Cryptography.UnitTests/Hashing/MapHasherTests.cs
@@ -0,0 +1,69 @@
+﻿using Formula.Cryptography.Hashing;
+using Formula.Cryptography.Utils;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Formula.Cryptography.UnitTests.Hashing
+{
+	public class MapHasherTests
+	{
+		[Fact]
+		public void Basic_Usage()
+		{
+			int count = 100000;
+			int min = 1;
+			int max = 1000;
+			
+			MapHasher hasher = new MapHasher();
+			int index = hasher.Hash("Some Data", 5); // Return a value from 0-4
+
+		}
+
+		[Fact]
+		public void HashBytes_Iterate_Verify_Ranges()
+		{
+			int count = 100000;
+			int min = 1;
+			int max = 1000;
+			List<int> indexes = new List<int>();
+			MapHasher hasher = new MapHasher();
+
+			for (int i = 0; i < count; i++)
+			{
+				int index = hasher.HashBytes(Guid.NewGuid().ToByteArray(), min, max);
+				indexes.Add(index);
+			}
+
+			// This code just to view the distibution of the indexes across the range
+			var results = indexes.GroupBy(i => i)
+								 .Select(g => new { Value = g.Key, Count = g.Count() })
+								 .OrderByDescending(x => x.Count);
+			results.ToList().ForEach(x => Trace.WriteLine($"{x.Value} count:{x.Count}"));
+
+			Assert.All(indexes, i => Assert.InRange(i, min, max));
+		}
+
+
+		[Fact]
+		public void MapHasher_MapAnyObject()
+		{
+			MapHasher hasher = new MapHasher();
+
+			byte[] bytes = CryptoUtils.GetRandomBytes(100);
+
+
+			int index = hasher.Hash("My Text To Hash", 0, 100);
+
+
+
+
+
+		}
+	}
+}
---
+++ Formula.Cryptography/Hashing/MapHasher.cs
@@ -0,0 +1,79 @@
+﻿using Formula.Cryptography.Utils;
+using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Drawing;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Formula.Cryptography.Hashing
+{
+	public class MapHasher
+	{
+		public MapHasher()
+		{
+		}
+
+		public int Hash(string input, int min, int max)
+		{
+			return HashBytes(CryptoUtils.StringToBytes(input), min, max);
+		}
+		public int Hash(string input, int size)
+		{
+			return HashBytes(CryptoUtils.StringToBytes(input), size);
+		}
+		public int HashBytes(byte[] bytes, int size)
+		{
+			return HashBytes(bytes, 0, size - 1);
+		}
+		public int HashBytes(byte[] bytes, int min, int max)
+		{
+			int hash = 0;
+			foreach (var b in bytes)
+			{
+				hash += (int)b;
+			}
+
+			int range = max - min + 1;
+			return (hash % range) + min;
+		}
+		public uint HashBytes2(byte[] bytes, uint min, uint max)
+		{
+			//This implementation uses the FNV-1a hash function to calculate the hash
+			//value of the input array, which is generally more robust than the
+			//simple sum used in the original implementation.It also caches the
+			//range value and uses the modulo operator more effectively to ensure
+			//that the resulting hash value is always within the desired range.
+
+			// Use the FNV-1a hash function to calculate the hash value
+			const uint fnvPrime = 0x01000193;
+			uint hash = 0x811C9DC5;
+			foreach (var b in bytes)
+			{
+				hash ^= b;
+				hash *= fnvPrime;
+			}
+
+			// Cache the range value
+			uint range = max - min + 1;
+
+			// Use the modulo operator more effectively
+			return (uint)((hash % range) + min - (hash % range < 0 ? range : 0));
+		}
+//		There are many different hashing algorithms that you can use as alternatives to the FNV-1a hash function.Here are a few examples:
+
+//MurmurHash: This is a fast, non-cryptographic hash function that is well-suited for hash-based data structures.It has a good distribution of hash values and is relatively resistant to hash collision attacks.
+
+//SHA-2: This is a family of cryptographic hash functions that are widely used for data integrity checks.SHA-2 functions are slower than non-cryptographic hash functions, but they provide a higher level of security and are suitable for use in situations where data integrity is important.
+
+//BLAKE2: This is a cryptographic hash function that is designed to be fast and secure. It has a variable-length output and can be used as a drop-in replacement for MD5, SHA-1, and other hash functions.
+
+//FarmHash: This is a fast, non-cryptographic hash function that is designed for use in distributed systems. It has a good distribution of hash values and is relatively resistant to hash collision attacks.
+
+
+
+
+	}
+}
